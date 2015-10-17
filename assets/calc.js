ae82811fc2b6afd72ca4b60c64ebe5d130fc4bf4
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2015-10-17 11:32:39 +0200

    Added menu

--- assets/calc.js
+++ assets/calc.js
@@ -1,1 +1,16 @@
-angular.module("calc");
+angular.module("calc", ['ngRoute'])
+
+.config(function($routeProvider){
+	$routeProvider.when('/menu', {
+		templateUrl: 'views/menu.htm'
+	}).when('/arithmetics', {
+		templateUrl: 'views/arithmetics.htm'
+	}).otherwise({redirectTo: '/menu'});
+})
+
+.directive("menu", function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'directives/menu.htm'
+	};
+});
