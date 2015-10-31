370966c9c41a2a1ad37d9e83c5a62308d574c177
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2015-10-31 21:28:48 +0100

    Use local version of bootstrap and font-awesome

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,35 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	ngdocs = require('gulp-ngdocs'),
 
+	config = {
+    	sassPath: 'assets/sass',
+    	bowerDir: 'bower_components/'
+	},
+
 	vendorJS 	= [
-				'bower_components/angular/angular.js',
-				'bower_components/angular-route/angular-route.js',
-				'bower_components/angular-bootstrap/ui-bootstrap.js'];
+				config.bowerDir + 'angular/angular.js',
+				config.bowerDir + 'angular-route/angular-route.js',
+				config.bowerDir + 'angular-bootstrap/ui-bootstrap.js'];
+
+	vendorCSS 	= [
+				config.bowerDir + 'bootstrap/dist/css/bootstrap.min.css',
+				config.bowerDir + 'font-awesome/css/font-awesome.min.css'];
 
 gulp.task('vendor_scripts', function() {
-  gulp.src(vendorJS)
+  return gulp.src(vendorJS)
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('assets'));
 });
 
+gulp.task('vendor_styles', function() {
+  return gulp.src(vendorCSS)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('assets/css'));
+});
 
-gulp.task('default', ['vendor_scripts']);
+gulp.task('vendor_fonts', function() {
+    return gulp.src(config.bowerDir + '/font-awesome/fonts/**.*')
+        .pipe(gulp.dest('assets/fonts'));
+});
+
+gulp.task('default', ['vendor_scripts', 'vendor_styles', 'vendor_fonts']);
